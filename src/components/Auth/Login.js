e15b56edf117237463c2419938955490f18b04de
unknown <ady.drozman2007@gmail.com>
2019-10-18 16:02:41 +0300

    create useFormValidation custom hook

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,15 +1,23 @@
 import React from "react";
+import useFormValidation from './useFormValidation'
+
+const INITIAL_STATE={
+  name:"",
+  email:"",
+  password:""
+}
 
 function Login(props) {
+  const {handleChange,handleSubmit,values}=useFormValidation(INITIAL_STATE)
   const [login,setLogin]=React.useState(true);
-
+ 
 
   return (<div>
     <h2 className="mv3">{login ?"Login" :"Create Account"}</h2>
-    <form className="flex flex-column">
-      {!login &&<input type="text" placeholder="Your Name" autoComplete="off" />}
-      <input type="email" placeholder="Your Email" autoComplete="off" />
-      <input type="password" placeholder="Choose a secure password" />
+    <form className="flex flex-column" onSubmit={handleSubmit}>
+      {!login &&<input type="text" placeholder="Your Name" autoComplete="off" onChange={handleChange} name="name" value={values.name} />}
+      <input type="email" placeholder="Your Email" autoComplete="off" onChange={handleChange} name="email" value={values.email} />
+      <input type="password" placeholder="Choose a secure password" onChange={handleChange} name="password" value={values.password} />
       <div className="flex mt3">
         <button type="submit" className="button pointer mr2">Submit</button>
         <button type="button" className="pointer button" onClick={()=>setLogin(prevLogin =>!prevLogin)}>{
--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,26 @@
 import React from "react";
 
-function useFormValidation() {}
+
+
+function useFormValidation(initialState) {
+    const [values,setValues]=React.useState(initialState);
+
+    function handleChange(event){
+        event.persist();
+        setValues(previosValues=>({
+            ...previosValues,
+            [event.target.name]:event.target.value
+        }))
+    }
+    function handleSubmit(event){
+        event.preventDefault()
+        console.log({values})
+    }
+    return {
+        handleChange,
+        handleSubmit,
+        values
+    }
+}
 
 export default useFormValidation;
